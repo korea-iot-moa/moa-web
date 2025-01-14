bfd779822821f30ffb2256f0232b2aecea67e649
Youngjun9909 <dydekd7511@gmail.com>
2025-01-14 17:39:32 +0900

    pyj

--- moa/src/layouts/GroupNaviBar/style.ts
+++ moa/src/layouts/GroupNaviBar/style.ts
@@ -1,19 +1,26 @@
 import { css } from "@emotion/react";
 
-export const naviBar = css`
-  position: relative;
+export const fullBox = css`
   box-sizing: border-box;
   border-radius: 10px 0px 0px 10px;
   height: 100%;
   width: 8%;
-  padding: 10px;
   overflow: hidden;
   background-color: #FF7B54;
   display: flex;
   flex-direction: column;
-  align-items: center;
   min-width: 80px;
   min-height: 730px;
+`;
+
+export const headerBox = css`
+  box-sizing: border-box;
+  width: 100%;
+  height: 80px;
+  display: flex;
+  padding: 10px 0;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const logoImage = css`
@@ -29,7 +36,9 @@ export const imageBox = css`
   box-sizing: border-box;
   padding: 5px;
   width: 60px;
+  min-width: 60px;
   height: 60px;
+  min-height: 60px;
   border-radius: 10%;
   overflow: hidden;
   background-color: #fff;
@@ -67,22 +76,33 @@ export const imageBox = css`
   }
 `;
 
-export const line = css`
-  border: none;
-  height: 1px;
-  background-color: #cfcfcf;
-  margin: 10px 0px;
+export const middleBox = css`
+  box-sizing: border-box;
   width: 100%;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px 0 ;
+  border-top: 1px solid #fff;
+  min-height: 0;
+  overflow-x: hidden;
+  overflow-y: scroll; 
+  scrollbar-width: none; 
+  -ms-overflow-style: none; 
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 export const bottomBox = css`
-  box-sizing: border-box;
   width: 100%;
-  height: 100px;
-  position: absolute;
-  padding: 10px;
-  bottom: 20px;
-
+  height: 80px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-top: 1px solid #fff;
 `;
 
 export const createBox = css`
