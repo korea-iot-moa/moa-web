b952f259c9feee54f8f589f7845e6b9e8434a73d
als5855 <wlals5855@naver.com>
2025-01-21 11:29:22 +0900

    20250121 jjm3

--- moa/src/views/Manager/GroupUpdate/style.ts
+++ moa/src/views/Manager/GroupUpdate/style.ts
@@ -1,78 +1,88 @@
 import { css } from "@emotion/react";
-export const Container = css `
-margin-top: 10px;
+
+export const totalContainer = css`
+  width: 70vh;
+  margin: 30px auto;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`
+
+export const Container = css`
+  margin-top: 10px;
 `;
 export const AllBox = css`
-display: flex;
-justify-content: space-evenly;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 15px;
 `;
 export const buttonBox = css`
   display: flex;
   flex-direction: row;
 `;
-export const TitleInput = css `
- width :70vh;
- height: 40px;
- font-size: 16px;
- font-weight: 500;
- padding-left: 15px;
- border-radius: 7px;
- text-align: left; 
-   resize: none;
+export const TitleInput = css`
+  width: 70vh;
+  height: 40px;
+  font-size: 16px;
+  font-weight: 500;
+  padding: 5px 10px;
+  border-radius: 7px;
+  text-align: left;
+  resize: none;
   display: block;
-  box-sizing: border-box; 
+  box-sizing: border-box;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
 
- `;
-
- export const ContentBox =css `
-  width :70vh;
+export const ContentBox = css`
+  width: 70vh;
   height: 180px;
   font-size: 16px;
   font-weight: 500;
   border-radius: 7px;
-  padding-left: 15px;
-  text-align: left; 
-   resize: none;
-   display: block;
-  box-sizing: border-box; 
- `;
+  padding: 10px;
+  text-align: left;
+  resize: none;
+  display: block;
+  box-sizing: border-box;
+`;
 
- 
-export const  bottomBox = css `
-display: flex;
-align-items:end;
+export const bottomBox = css`
+  display: flex;
+  align-items: end;
 `;
- 
+
 export const DateContainer = css`
-display:flex; 
-justify-content:center;
+  display: flex;
+  justify-content: center;
 `;
-export const DateBox = css `
-  width :30%;
+export const DateBox = css`
+  width: 30%;
   height: 40px;
-  text-align:center;
+  text-align: center;
   border-radius: 7px;
 `;
 
-export const Tab =css `
+export const Tab = css`
   font-family: "IBM Plex Sans", sans-serif;
   color: #000;
   cursor: pointer;
-  font-size: 16px;
+  font-size: 11px;
   font-weight: 600;
-  background-color:rgb(255, 128, 10);
+  background-color: rgb(255, 128, 10);
   width: 20%;
   padding: 10px 12px;
-  margin: 10px;
   border: none;
   border-radius: 7px;
   display: flex;
   justify-content: space-evenly;
-  box-sizing: border-box; 
+  box-sizing: border-box;
   transition: none;
 
   &:hover {
-    background-color:rgb(245, 190, 140);
+    background-color: rgb(245, 190, 140);
   }
 
   &:focus {
@@ -88,10 +98,11 @@ export const activeTab = css`
   width: 20%;
   border: none;
   border-radius: 7px;
+  font-size: 11px;
   cursor: pointer;
 `;
 export const rowFieldBox = css`
-  box-sizing: border-box ;
+  box-sizing: border-box;
   display: flex;
   flex-direction: column;
   width: 47%;
@@ -101,9 +112,8 @@ export const rowFieldBox = css`
 export const labelBox = css`
   display: flex;
   align-items: center;
-  flex-direction:row;
+  flex-direction: row;
   margin-bottom: 5px;
-
 `;
 
 export const genderBox = css`
@@ -151,7 +161,6 @@ export const genderBox = css`
     flex: 1;
     text-align: center;
 
-
     &:hover {
       border: 1px solid #4d4d4d;
       background-color: #4d4d4d;
@@ -167,27 +176,27 @@ export const label = css`
   margin: 0;
 `;
 
-export const MoveButton =css `
-font-family: "IBM Plex Sans", sans-serif;
-color: #000;
-cursor: pointer;
-font-size: 16px;
-font-weight: 600;
-background-color:rgb(255, 132, 25);
-width: 20%;
-padding: 10px 12px;
-margin: 10px;
-border: none;
-border-radius: 7px;
-display: flex;
-justify-content: space-evenly;
+export const MoveButton = css`
+  font-family: "IBM Plex Sans", sans-serif;
+  color: #000;
+  cursor: pointer;
+  font-size: 16px;
+  font-weight: 600;
+  background-color: rgb(255, 132, 25);
+  width: 20%;
+  padding: 10px 12px;
+  margin: 10px;
+  border: none;
+  border-radius: 7px;
+  display: flex;
+  justify-content: space-evenly;
 
-&:hover {
-  background-color: #fab77d;
-}
+  &:hover {
+    background-color: #fab77d;
+  }
 
-&:focus {
-  color: #fff;
-  outline: 3px solid #DAE2ED;
-}
+  &:focus {
+    color: #fff;
+    outline: 3px solid #dae2ed;
+  }
 `;
