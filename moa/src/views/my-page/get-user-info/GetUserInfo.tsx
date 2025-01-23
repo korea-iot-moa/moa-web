cbe2bb4e57bd6cc26387721afdeae733740157bb
als5855 <wlals5855@naver.com>
2025-01-23 17:50:00 +0900

    20250123 jjm

--- moa/src/views/my-page/get-user-info/GetUserInfo.tsx
+++ moa/src/views/my-page/get-user-info/GetUserInfo.tsx
@@ -29,7 +29,7 @@ const GetUserInfo = () => {
     useState<boolean>(false);
   const [duplicatoinNickNameMs, setDuplicationNickNameMs] =
     useState<string>("");
-
+  
   useEffect(() => {
     fetchUserInfo();
   }, [navigate]);
